4a418677516c926cb3a79fec4ad945c7d018aaf2
Mahmoud Abdelraouf <mahmoud126125#gmail.com>
2023-12-20 20:31:42 +0200

    Update the driver

--- 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/CascadeController.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/CascadeController.v
@@ -1,26 +1,37 @@
-module cascade_controller(
-    inout [2:0]CAS,
-    input SP,
-    input INTA,
-    input [2:0]PRIORITY_PIN,
-    input CMD,
-    input ID,
-    output VECTOR,
-);
-localparam SLAVE=1'b0;
-localparam MASTER=1'b1;
+/**
+ * @file cascade_controller.v
+ * @brief Cascading Controller Module for Interrupt Handling
+ */
 
-always @(negedge INTA) begin
-    case (SP):
-        SLAVE: begin
-            if(ID==CAS)begin
-                //send vector to data bus if the CAS matchs the current slave ID
+/**
+ * @brief Cascade Controller Module
+ * This module manages the cascading functionality for interrupt handling within a microcontroller.
+ * It interfaces with slave devices based on priority pins and manages the transmission of interrupt vectors.
+ */
+module cascade_controller(
+    inout [2:0] CAS,             // Input/Output: Cascade control lines.
+    input SP,                    // Input: Selects between MASTER and SLAVE modes.
+    input INTA,                  // Input: Interrupt acknowledge signal.
+    input [2:0] PRIORITY_PIN,    // Input: Priority pins specifying slave IDs.
+    input CMD,                   // Input: Command signal (unused in current implementation).
+    input ID,                    // Input: ID signal to identify slave devices.
+    output VECTOR                // Output: Interrupt vector to be sent to the data bus.
+);
+    localparam SLAVE = 1'b0;     // Local parameter representing the SLAVE mode.
+    localparam MASTER = 1'b1;    // Local parameter representing the MASTER mode.
+
+    // Cascade Handling Logic
+    always @(negedge INTA) begin
+        case (SP):
+            SLAVE: begin
+                if (ID == CAS) begin
+                    // Send vector to data bus if the CAS matches the current slave ID
+                end
             end
-        end
-        MASTER:begin
-            //Send the slave ID we want
-            CAS<=PRIORITY_PIN;
-        end
-    endcase
-end
+            MASTER: begin
+                // Send the slave ID we want
+                CAS <= PRIORITY_PIN;
+            end
+        endcase
+    end
 endmodule
--- 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/IMR.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/IMR.v
@@ -1,29 +1,35 @@
 /**
-* @brief Interrupt Mask Register (IMR) 8-bit register which contains 
-* the interrupt request lines which are masked.
-* 
-* - This module is conneted to the OCW1 line to know which IR is masked.
-* - Connected to the control logic with writeIMR signal, if it is active, the IMW 
-* valuse are printed to the dataBuffer so the cpu read it.
-* - bitToMask is connected to the IRR module, if a bit is one, than the corresponding 
-* IR is masked and rejected.
-* - dataBuffer lines are connected to the Data Buffer module.
-*/
+ * @file IMR.v
+ * @brief Interrupt Mask Register (IMR) 8-bit register module
+ * @details This module represents an 8-bit register that manages masked interrupt request lines.
+ * It provides functionality to mask specific interrupts and interfaces with various components 
+ * within the system.
+ */
+
+/**
+ * @brief IMR module
+ * @details The Interrupt Mask Register (IMR) module contains an 8-bit register responsible for 
+ * storing masked interrupt request lines. It interfaces with the OCW1 line to determine masked 
+ * interrupts, the writeIMR signal to update the dataBuffer for CPU access, and the IRR module 
+ * through bitToMask for rejecting masked interrupts.
+ */
 module IMR(
   input wire [7:0] OCW1,
   input wire writeIMR,
   output wire [7:0] bitToMask,
   output reg [7:0] dataBuffer
 );
-//compinational circuit.
-assign bitToMask = OCW1;
 
-//Async circuit.
-always@ (writeIMR) begin
-  if(writeIMR == 1)
-    dataBuffer <= OCW1;
-  else
-    dataBuffer <= 8'bz;
-end
+  // Combinational logic to assign bitToMask directly from OCW1
+  assign bitToMask = OCW1;
+
+  // Asynchronous logic for updating dataBuffer based on writeIMR
+  always @* begin
+    if (writeIMR) begin
+      dataBuffer = OCW1;
+    end else begin
+      dataBuffer = 8'bZ; // High-impedance state when writeIMR is inactive
+    end
+  end
 
 endmodule
