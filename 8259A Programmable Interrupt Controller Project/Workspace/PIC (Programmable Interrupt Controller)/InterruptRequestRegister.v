46227d89114973beb4fb16fbf6d9b4fefd6b590c
Mahmoud Abdelraouf <mahmoud126125#gmail.com>
2023-12-21 00:08:02 +0200

    Update the IRR

--- 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/InterruptRequestRegister.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/InterruptRequestRegister.v
@@ -1,73 +1,45 @@
+/**
+ * @file irr.v
+ * @brief Interrupt Request Register (IRR) module for interrupt handling
+ */
+
+/**
+ * @brief Interrupt Request Register (IRR) Module
+ * @details Manages the Interrupt Request Register to handle interrupt requests and priority resolution.
+ * It interfaces with the Interrupt Mask Register (IMR) and control logic for interrupt servicing.
+ */
 module InterruptRequestRegister (
-  input clk,                             // Clock input
-  input [7:0] event_condition,           // Conditions indicating events from the peripherals
-  input [7:0] enable_pie,                // Enable signal for Peripheral Interrupt Enable
-  input enable_gie,                      // Enable signal for General Interrupt Enable
-  output reg interrupt,                  // Final interrupt signal
-  output reg [7:0] valid_interrupts      // Latch to store indices of valid interrupts
+    input [7:0] IR0_to_IR7,            // Input: Interrupt requests from IR0 to IR7.
+    input [7:0] bitToMask,             // Input: Masking bits from IMR for corresponding IRs.
+    input readPriority,                // Input: Read priority signal from control logic.
+    input readIRR,                     // Input: Signal to output IRR values to data buffer.
+    input resetIRR,                    // Input: Signal from priority resolver to reset serviced interrupts.
+    output reg [7:0] risedBits         // Output: Rised bits indicating valid interrupts.
 );
 
-  reg [7:0] pif;                         // Peripheral Interrupt Flags
-  reg [7:0] pie;                         // Peripheral Interrupt Enable
-  reg gie;                               // General Interrupt Enable
+    // Internal register to hold the state of interrupts
+    reg [7:0] interruptState;
 
-  // Latch to store the indices of valid interrupts
-  reg [7:0] valid_interrupts_latch;
+    // Logic to handle valid interrupts
+    always @* begin
+        // Combine interrupt requests with mask bits to find valid interrupts
+        // Valid interrupts have a '0' in bitToMask corresponding to '1' in IR signals
+        interruptState = IR0_to_IR7 & ~bitToMask;
 
-  // Peripheral module to generate PIF signals for 8 peripherals
-  genvar i;
-  generate
-    for (i = 0; i < 8; i = i + 1) begin : periph_instances
-      PeripheralInterruptFlag peripheral_interrupt_flag_inst (
-        .clk(clk),
-        .event_condition(event_condition[i]),
-        .pif(pif[i])
-      );
-    end
-  endgenerate
-
-  // PeripheralController module to manage PIE signal for all peripherals
-  generate
-    for (i = 0; i < 8; i = i + 1) begin : periph_controller_instances
-      PeripheralInterruptEnable peripheral_controller_inst (
-        .clk(clk),
-        .enable_pie(enable_pie[i]),
-        .pie(pie[i])
-      );
-    end
-  endgenerate
-
-  // InterruptController module to manage GIE signal
-  GlobalInterruptEnable general_interrupt_enable_inst (
-    .clk(clk),
-    .enable_gie(enable_gie),
-    .gie(gie)
-  );
-
-  // Combined logic to generate interrupt when all conditions are met for enabled peripherals
-  always @(posedge clk) begin
-    if (enable_gie) begin
-      if ((|pie) && (|pif) && gie) begin
-        interrupt <= 1;
-      end else begin
-        interrupt <= 0;
-      end
-    end else begin
-      interrupt <= 0;
-    end
-
-    // Store indices of valid interrupts in the latch
-    if (enable_gie) begin
-      valid_interrupts_latch <= 8'b0;
-      for (i = 0; i < 8; i = i + 1) begin
-        if (enable_pie[i] && pif[i]) begin
-          valid_interrupts_latch[i] <= 1;
+        // Logic to reset serviced interrupts based on readPriority and resetIRR signals
+        if (readPriority && resetIRR) begin
+            // Reset corresponding interrupts in IRR based on priority resolution
+            interruptState = interruptState & ~readPriority;
         end
-      end
     end
-  end
 
-  // Assign the latch to the output
-  assign valid_interrupts = valid_interrupts_latch;
+    // Logic to output valid interrupts when readIRR is asserted
+    always @* begin
+        if (readIRR) begin
+            risedBits = interruptState;
+        end else begin
+            risedBits = 8'b0; // Output '0' when not reading the IRR
+        end
+    end
 
 endmodule
