a8ecec584c281fade637a6f35f4738dfbcd3c1d2
Mahmoud Abdelraouf <mahmoud126125#gmail.com>
2023-12-21 00:59:08 +0200

    Update the IRR

--- 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/InterruptRequestRegister.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC (Programmable Interrupt Controller)/InterruptRequestRegister.v
@@ -13,32 +13,47 @@ module InterruptRequestRegister (
     input [7:0] bitToMask,             // Input: Masking bits from IMR for corresponding IRs.
     input readPriority,                // Input: Read priority signal from control logic.
     input readIRR,                     // Input: Signal to output IRR values to data buffer.
-    input resetIRR,                    // Input: Signal from priority resolver to reset serviced interrupts.
-    output reg [7:0] risedBits         // Output: Rised bits indicating valid interrupts.
+    input [2:0] resetIRR,              // Input: Signal from priority resolver to reset serviced interrupts.
+    output reg [7:0] risedBits,        // Output: Rised bits indicating valid interrupts.
+    output reg [7:0] dataBuffer        // Output: Buffer for interrupts reset by resetIRR.
 );
 
-    // Internal register to hold the state of interrupts
+    // Internal register to hold the previous state of interrupts
+    reg [7:0] prevInterruptState;
+
+    // Internal register to hold the current state of interrupts
     reg [7:0] interruptState;
 
-    // Logic to handle valid interrupts
-    always @* begin
+    // Logic to handle valid interrupts and reset based on readPriority and resetIRR signals
+    always @(*) begin
         // Combine interrupt requests with mask bits to find valid interrupts
         // Valid interrupts have a '0' in bitToMask corresponding to '1' in IR signals
         interruptState = IR0_to_IR7 & ~bitToMask;
 
-        // Logic to reset serviced interrupts based on readPriority and resetIRR signals
-        if (readPriority && resetIRR) begin
-            // Reset corresponding interrupts in IRR based on priority resolution
-            interruptState = interruptState & ~readPriority;
+        // Resetting interrupts based on readPriority and resetIRR signals
+        if (readPriority) begin
+            // Reset corresponding interrupts in IRR based on priority resolution and resetIRR value
+            if (resetIRR != 0) begin
+                interruptState = interruptState & ~(1 << resetIRR);
+            end else begin
+                interruptState[0] = 0; // Reset IR0 when resetIRR is '0'
+            end
         end
     end
 
-    // Logic to output valid interrupts when readIRR is asserted
-    always @* begin
+    // Storing interrupts in dataBuffer when readIRR is asserted
+    always @(*) begin
         if (readIRR) begin
+            dataBuffer = interruptState;
+        end
+    end
+
+    // Logic to output valid interrupts when readIRR is asserted or when there's a change in interruptState
+    always @(*) begin
+        if (readIRR || (interruptState != risedBits)) begin
             risedBits = interruptState;
         end else begin
-            risedBits = 8'b0; // Output '0' when not reading the IRR
+            risedBits = 8'bZ; // Preserve previous risedBits if there are no changes or if readIRR is inactive
         end
     end
 
