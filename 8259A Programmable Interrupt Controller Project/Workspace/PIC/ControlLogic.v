b0fc792d117a5e506b7ca3d22ece2e59e550fbe5
Ahmed Khaled <130765437+Ahmed-Khaled-Abdelmaksod@users.noreply.github.com>
2023-12-21 19:42:59 +0200

    Update ControlLogic.v(add features)

--- 8259A Programmable Interrupt Controller Project/Workspace/PIC/ControlLogic.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC/ControlLogic.v
@@ -4,7 +4,11 @@ module ControlLogic(
     input cascade_flag,                     // Input: In case of slave, it will be sent from cascade in case of desired slave
     input read_cmd_to_ctrl_logic,           // Input: Sent from read/write logic to read status of ISR or IRR or priority
     input [7:0] OCW3,                       // Input: Will be used to know which register to read its status 
-        
+    input write_flag,                       // Input: to indicate that there are writing operation 
+    input [2:0] interrupt_index,
+    input SP,
+    input [7:0]ICW3,
+
     output reg read_IRR,                    // Output: Signal to read IRR status ( will be sent to IRR)
     output reg read_priority,               // Output: Is set after the first INTA pulse (will be sent to IRR and ISR)
     output reg freezing,                    // Output: Is set between to INTA pulse.
@@ -13,6 +17,7 @@ module ControlLogic(
     output reg INT,                         // Output: Interrupt request will be sent to CPU
     output reg cascade_signal,              // Output: Signal will be sent to cascade controller to start working
     output reg desired_slave                // Output: Slave ID that will be sent to cascade controller in case of master
+    output reg send_vector_isr 
 );
    // Configurations for read_ISR and read_IRR flags
     localparam read_from_ISR =2'b11;        // Local parameter representing read from ISR mode
@@ -27,12 +32,12 @@ module ControlLogic(
         // Decide what flag to set based on coming signal 
         case(read_register)
             read_from_ISR: begin
-                read_ISR<=1; // set read from ISR flag to send ISR register
-                read_IRR<=0; // reset read from IRR flag 
+                read_ISR<=1'b1; // set read from ISR flag to send ISR register
+                read_IRR<=1'b0; // reset read from IRR flag 
             end
             read_from_IRR: begin
-                read_IRR<=1; // set read from IRR flag to send IRR register
-                read_ISR<=0; // reser read from ISR flag
+                read_IRR<=1'b1; // set read from IRR flag to send IRR register
+                read_ISR<=1'b0; // reser read from ISR flag
             end
         endcase 
     end
@@ -44,16 +49,42 @@ module ControlLogic(
     always@(negedge INTA)begin
         // if it's the first INTA set read_priority and freezing flags
         if(counter==first_INTA)begin
-            read_priority<=1;
-            freezing<=1;
+            read_priority<=1'b1;
+            freezing<=1'b1;
             counter=counter+1;
         end
         // if it't the second INTA reset read_priority and freezing flags
         else if(counter==second_INTA)begin
-            read_priority<=0;
-            freezing<=0;
-            counter=0;
+            read_priority<=1'b0;
+            freezing<=1'b0;
+            counter=1'b0;
         end
     end
+
+    // Handling INT request which will be sent to Processor
+    // if there are any reading or writing operations we cann't send any interrupt
+    always@(posedge INT_request)
+    begin
+        if(write_flag == 1'b0 && read_cmd_to_ctrl_logic == 1'b0)
+        begin
+            INT <= 1'b1;
+        end
+    end
+    localparam Slave = 1'b0;
+    localparam Master = 1'b1;
+    always@(*)
+    begin
+        if(Slave == SP)
+        begin
+            send_vector_isr <= 1'b1;
+        end
+        else if (Master == SP)
+        begin
+            case(interrupt_index)
+                // 110 -> isr / *****************
+            endcase
+        end
+    end
+
 
 endmodule
