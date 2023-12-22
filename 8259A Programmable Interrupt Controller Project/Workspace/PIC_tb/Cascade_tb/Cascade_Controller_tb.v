66c8c4b9bf528fbfc0609951fad1097940879721
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2023-12-22 14:51:53 +0200

    Update Cascade_Controller_tb.v

--- 8259A Programmable Interrupt Controller Project/Workspace/PIC_tb/Cascade_tb/Cascade_Controller_tb.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC_tb/Cascade_tb/Cascade_Controller_tb.v
@@ -1,3 +1,4 @@
+
 `timescale 1ns / 1ps
 
 module CascadeController_tb;
@@ -5,11 +6,11 @@ module CascadeController_tb;
   // Inputs
   reg SP1,SP2,SP3;
   reg [7:0] ICW3_1 , ICW3_2 ,ICW3_3   ;
-  reg control_signal;
+  reg control_signal_1 ,control_signal_2, control_signal_3;
   reg [2:0] desired_slave;
 
   // Outputs
-  reg [2:0] CAS;
+  wire [2:0] CAS;
   wire flag,flag2,flag3;
   
   
@@ -18,7 +19,7 @@ CascadeController Pic1 (
   .CAS(CAS),
   .SP(SP1),
   .ICW3(ICW3_1),
-  .control_signal(control_signal),
+  .control_signal(control_signal_1),
   .desired_slave(desired_slave),
   .flag(flag)
 );
@@ -28,7 +29,7 @@ CascadeController Pic2 (
   .CAS(CAS),
   .SP(SP2),
   .ICW3(ICW3_2),
-  .control_signal(control_signal),
+  .control_signal(control_signal_2),
   .desired_slave(desired_slave),
   .flag(flag2)
 );
@@ -38,7 +39,7 @@ CascadeController Pic3 (
   .CAS(CAS),
   .SP(SP3),
   .ICW3(ICW3_3),
-  .control_signal(control_signal),
+  .control_signal(control_signal_3),
   .desired_slave(desired_slave),
   .flag(flag3)
 );
@@ -49,51 +50,63 @@ initial begin
   // Initialize inputs for Pic1
   SP1 = 1; // Pic1 operates in MASTER Mode
   SP2=0 ; // Pic2 is a slave
-  SP2=0 ; // Pic3 is a slave 
+  SP3=0 ; // Pic3 is a slave 
   ICW3_1 = 8'b00000000; // The last 3 bits are 000 Which is the casecade ID of this pic (But it is a Master)
   ICW3_2 = 8'b00000110; // Cascade ID 110
   ICW3_3 = 8'b00000111; // Cascade ID 111
   
-  control_signal = 0; // Initial value for control signal
-  
+  control_signal_1 = 0; // Initial value for control signal
+  control_signal_2 = 0;
+  control_signal_3 = 0;
   
   desired_slave = 3'b010; // Firstly we are goning to choose an unavailable slave 
 
-  control_signal = 1; // Nothing should occure here because and the CAS 010 (set by Master) doesn't match any slave (only avaialbe is 110 & 111)
+  control_signal_1 = 1; // Nothing should occure here because and the CAS 010 (set by Master) doesn't match any slave (only avaialbe is 110 & 111)
+  control_signal_2 = 1;
+  control_signal_3 = 1;
+  
+  
   #100;
 
-  // Apply a control signal rising edge
-  control_signal = 0; // The flag of the slave should rise
+  // Turning off the operation
+  control_signal_1 = 0;
+  control_signal_2 = 0;
+  control_signal_3 = 0;
 
-  #10;
-
-  control_signal = 0; // Turning off the operation
 
   // Now , choosing Pic 2
   desired_slave = 3'b110; // The last three bits are 110 which matches CAS[2:0] of the instance pic2
 
-  control_signal = 1; // Flag2 of Pic2 should arise and the CAS 110 (set by Master) ID of PIC2
+  control_signal_1 = 1; // Flag2 of Pic2 should arise and the CAS 110 (set by Master) ID of PIC2
+  control_signal_2 = 1;
+  control_signal_3 = 1;
+  
   #100; 
 
-  // Apply a control signal rising edge
-  control_signal = 0; // The flag of the slave should rise
+  // Turning off the operation
+  control_signal_1 = 0;
+  control_signal_2 = 0;
+  control_signal_3 = 0;
+
 
   #10;
-
-  control_signal = 0; // Turning off the operation
 
   // Now , choosing Pic 3
   desired_slave = 3'b111; // The last three bits are 111 which matches CAS[2:0] of the instance Pic3
 
-  control_signal = 1; // Flag3 of Pic3 should arise as the CAS 111 (set by Master) ID of PIC2
+  control_signal_1 = 1; // Flag3 of Pic3 should arise as the CAS 111 (set by Master) ID of PIC2
+  control_signal_2 = 1;
+  control_signal_3 = 1;
   #100; 
 
-  control_signal = 0; // Turning off the operation
+  // Turning off the operation
+  control_signal_1 = 0;
+  control_signal_2 = 0;
+  control_signal_3 = 0;
 
   #10;
 
-  // Terminate the simulation
-  $finish;
+  
 end
 
 
