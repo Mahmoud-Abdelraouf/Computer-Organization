3d2654ab3ab4e151ed184625d9564cfd3351d78e
SemoMoh <128331219+SemoMoh@users.noreply.github.com>
2023-12-23 00:30:36 +0200

    IMR testbench

--- 8259A Programmable Interrupt Controller Project/Workspace/PIC_tb/IMR_tb/InterruptMaskRegister.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC_tb/IMR_tb/InterruptMaskRegister.v
@@ -13,7 +13,7 @@ module InterruptMaskRegister(
   input wire [7:0] OCW1, //OCW1 commands to know which bits are masked. conected to the R/D logic.
   input wire readIMR, // To put the IMR_reg into the internal data lines, connected to the control logic.
   output wire [7:0] IMR_reg, //IMR (status) register, connected to IRR.
-  output reg [7:0] = 8'bz // Internal data bus that is connected to the data buffer.
+  output reg [7:0] dataBuffer = 8'bz // Internal data bus that is connected to the data buffer.
 );
 //compinational circuit.
 assign IMR_reg = OCW1;
---
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC_tb/IMR_tb/work/@interrupt@mask@register_tb/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity InterruptMaskRegister_tb is
+end InterruptMaskRegister_tb;
