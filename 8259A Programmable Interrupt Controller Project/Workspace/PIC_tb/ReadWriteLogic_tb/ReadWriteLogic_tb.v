fa06865107fc46f9463bbfeca5f5e9660cc98cd2
Ahmed Khaled <130765437+Ahmed-Khaled-Abdelmaksod@users.noreply.github.com>
2023-12-24 01:49:33 +0200

    Update ReadWriteLogic_tb.v

--- 8259A Programmable Interrupt Controller Project/Workspace/PIC_tb/ReadWriteLogic_tb/ReadWriteLogic_tb.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/PIC_tb/ReadWriteLogic_tb/ReadWriteLogic_tb.v
@@ -1,4 +1,3 @@
-`timescale 1ns/1ps
 module ReadWriteLogic_tb;
     reg Read;
     reg write;
@@ -16,23 +15,30 @@ module ReadWriteLogic_tb;
     wire [7:0]OCW2;
     wire [7:0]OCW3;
     wire read_cmd_to_ctrl_logic;
+    wire read_cmd_imr_to_ctrl_logic;
 
-    ReadWriteLogic RW(.Read(Read),.write(write),.A0(A0),.CS(CS),.dataBuffer(dataBuffer),.write_flag_ACK(write_flag_ACK),
+    
+
+    ReadWriteLogic readWriteLogic(.Read(Read),.write(write),.A0(A0),.CS(CS),.dataBuffer(dataBuffer),.write_flag_ACK(write_flag_ACK),
     .write_flag(write_flag),.ICW1(ICW1),.ICW2(ICW2),.ICW3(ICW3),.ICW4(ICW4),.OCW1(OCW1),.OCW2(OCW2),.OCW3(OCW3),
-    .read_cmd_to_ctrl_logic(read_cmd_to_ctrl_logic));
+    .read_cmd_to_ctrl_logic(read_cmd_to_ctrl_logic),.OCW3_change(OCW3_change),.read_cmd_imr_to_ctrl_logic(read_cmd_imr_to_ctrl_logic)
+    );
+    
 
     initial begin
+        //ICW1
         write = 1;
         CS =0;
         A0 = 0;
         Read = 1;
-        dataBuffer = 8'b00010001;
+        dataBuffer = 8'b00010000;
         #100;
         write = 0;
         CS =0;
         A0 = 0;
         Read = 1;
-        dataBuffer = 8'b00010001;
+        dataBuffer = 8'b00010000;
+//ICW2
         #100;
         write = 1;
         CS =0;
@@ -45,13 +51,42 @@ module ReadWriteLogic_tb;
         A0 = 1;
         Read = 1;
         dataBuffer = 8'b00010111;
+//ICW3
+/*
+        #100;
+        write = 1;
+        CS =0;
+        A0 = 1;
+        Read = 1;
+        dataBuffer = 8'b00010111;
+        #100;
+        write = 0;
+        CS =0;
+        A0 = 1;
+        Read = 1;
+        dataBuffer = 8'b00010111;
+*/
+//ICW4
+        #100;
+        write = 1;
+        CS =0;
+        A0 = 1;
+        Read = 1;
+        dataBuffer = 8'b00010111;
+        #100;
+        write = 0;
+        CS =0;
+        A0 = 1;
+        Read = 1;
+        dataBuffer = 8'b00010111;
+
+// OCW1
         #100;
         write = 1;
         CS =0;
         A0 = 1;
         Read = 1;
         dataBuffer = 8'b01010101;
-        
         #100;
         write = 0;
         CS =0;
@@ -59,19 +94,118 @@ module ReadWriteLogic_tb;
         Read = 1;
         dataBuffer = 8'b01010101;
 
-        
+// OCW2        
         #100;
+        write = 1;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b10000111;
+        #100;
+        write = 0;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b10000111;
+        #100;
+// OCW3
+        write = 1;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b11101111;
+        #100;
+        write = 0;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b11101111;
+        #100;
+
+        //OCW1
         write = 1;
         CS =0;
         A0 = 1;
         Read = 1;
-        dataBuffer = 8'b10001111;
-
+        dataBuffer = 8'b11101111;
         #100;
         write = 0;
         CS =0;
         A0 = 1;
         Read = 1;
-        dataBuffer = 8'b10001111;
+        dataBuffer = 8'b11101111;
+        #100;
+
+        //OCW3
+        write = 1;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b00101111;
+        #100;
+        write = 0;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b00101111;
+        #100;
+
+        
+        // OCW2        
+        
+        write = 1;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b11100111;
+        #100;
+        write = 0;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b11100111;
+        #100;
+        //////////////////////////////////////////////////////////////////
+        // test Reading operations isr || irr //
+        write = 1;
+        CS =0;
+        A0 = 0;
+        Read = 1;
+        dataBuffer = 8'b11100111;
+        #100;
+        write = 1;
+        CS =0;
+        A0 = 0;
+        Read = 0;
+        dataBuffer = 8'b11100111;
+        #100;
+
+        // read imr
+        write = 1;
+        CS =0;
+        A0 = 1;
+        Read = 1;
+        dataBuffer = 8'b11100111;
+        #100;
+        write = 1;
+        CS =0;
+        A0 = 1;
+        Read = 0;
+        dataBuffer = 8'b11100111;
+        #100;
+
+        write = 1;
+        CS =0;
+        A0 = 1;
+        Read = 1;
+        dataBuffer = 8'b11100111;
+        #100;
+        write = 1;
+        CS =0;
+        A0 = 1;
+        Read = 0;
+        dataBuffer = 8'b11100111;
+        #100;
+
     end
 endmodule
