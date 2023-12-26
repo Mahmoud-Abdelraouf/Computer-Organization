f4085eeba0b41da2e58edec4399cf753196748dd
adhamkhaled312 <105177075+adhamkhaled312@users.noreply.github.com>
2023-12-27 00:11:09 +0200

    Add documentation for control logic

--- 8259A Programmable Interrupt Controller Project/Workspace/Modelsim/PIC/ControlLogic.v
+++ 8259A Programmable Interrupt Controller Project/Workspace/Modelsim/PIC/ControlLogic.v
@@ -35,50 +35,53 @@ module ControlLogic(
     
     output reg EOI_to_cascade,              // Output: Signal to cascade controller to reset cascade lines(in case of cascading mode and master).
     output reg cascade_signal,              // Output: Signal to cascade controller to start working (Master mode).
-    output reg [2:0] desired_slave,          // Output: Slave ID that will be sent to cascade controller (Master mode).
+    output reg [2:0] desired_slave,         // Output: Slave ID that will be sent to cascade controller (Master mode).
     output reg cascade_flag_ACK=1'b0        // Output: Acknowledge for cascade flag (slave mode).
 );
    // Configurations for read_ISR and read_IRR flags
     localparam read_from_ISR =2'b11;        // Local parameter representing read from ISR mode
     localparam read_from_IRR =2'b01;        // Local parameter representing read from IRR mode
-    reg [1:0] read_register;    
+    reg [1:0] read_register;
+    // Determine which register to read from LS 3 bits    
     always @(OCW3)begin
-        read_register=OCW3[1:0]; 
+        read_register=OCW3[1:0];
     end
     
     // Do configurations when recieving signal from read/write logic
     always @(posedge read_cmd_to_ctrl_logic) begin
-        // Decide what flag to set based on coming signal 
+        // Decide what flag to set based on OCW3 signal 
         case(read_register)
             read_from_ISR: begin
-                read_ISR<=1'b1; // set read from ISR flag to send ISR register
+                read_ISR<=1'b1; // set read from ISR flag
                 read_IRR<=1'b0; // reset read from IRR flag 
             end
             read_from_IRR: begin
-                read_IRR<=1'b1; // set read from IRR flag to send IRR register
+                read_IRR<=1'b1; // set read from IRR flag
                 read_ISR<=1'b0; // reset read from ISR flag
             end
             default: begin
-                read_IRR<=1'b0; // set read from IRR flag to send IRR register
+                read_IRR<=1'b0; // reset read from IRR flag
                 read_ISR<=1'b0; // reset read from ISR flag
             end
         endcase 
     end
     
-    // deactive flags to stop reading the IRR and ISR Registers
+    // Deactive flags to stop reading the IRR and ISR Registers
     always @(negedge read_cmd_to_ctrl_logic) begin
         read_ISR<=1'b0;
         read_IRR<=1'b0;
     end
 
-    // block to handle reading from imr register 
+    // Block to handle reading from IMR register when recieving signal from read/write logic
     always @(read_cmd_imr_to_ctrl_logic)
     begin
-        if(read_cmd_imr_to_ctrl_logic) // read operation active 
+        // Activate read from IMR
+        if(read_cmd_imr_to_ctrl_logic)
         begin
             read_IMR <= 1'b1;
         end
-        else // deactive read operation
+        // Deactivate read from IMR
+        else
         begin
             read_IMR <= 1'b0;
         end
@@ -87,7 +90,8 @@ module ControlLogic(
     // Configurations for freezing and read_priority flags
     localparam first_INTA=1'b0;         // Local parameter representing first INTA
     localparam second_INTA=1'b1;        // Local parameter representing second INTA
-    reg counter=0; //  counter for first and second INTA
+    reg counter=0; //  Counter to determine first and second INTA
+    // Do logic based on first or second acknowledge from CPU
     always@(negedge INTA)begin
         // if it's the first INTA set read_priority and freezing flags
         if(counter==first_INTA)begin
@@ -106,27 +110,32 @@ module ControlLogic(
     end
 
     // Handling INT request which will be sent to Processor
-    // if there are any reading or writing operations we cann't send any interrupt
-    always@(posedge INT_request)
-    begin
-        if(write_flag == 1'b0 && read_cmd_to_ctrl_logic == 1'b0)
+    // if there is any reading or writing operations we can't send any interrupt
+    always@(posedge INT_request)begin
+        // Set INT to cpu only if cpu isn't writing on PIC and ther's no reading status from any register
+        if(write_flag == 1'b0 && read_cmd_to_ctrl_logic == 1'b0 && read_cmd_imr_to_ctrl_logic==1'b0)
         begin
             INT <= 1'b1;
         end
-        INT_request_ACK = ~INT_request_ACK;
+        INT_request_ACK = ~INT_request_ACK; // Send acknowledge for INT_request flag
     end
     always @(negedge INT_request) begin
         INT <= 1'b0;
     end
+    // At end of interrupt send flag to cascade controller to set cascade lines to 3'bzzz to be ready for next interrupt
+    // and reset INT flag.
     always @(posedge EOI)begin
         EOI_to_cascade=EOI;
         INT=0;
     end
-
-    localparam MASTER=1'b1;
-    localparam SLAVE=1'b0;
-    always@(posedge second_ACK)
+    
+    // Handling sending vector on data buffer for CPU
+    localparam MASTER=1'b1;     // Local parameter representing current PIC is master
+    localparam SLAVE=1'b0;      // Local parameter representing current PIC is slave
+    always@(posedge second_ACK) // Start configurations on the secont INTA
     begin
+        // Check bit no. 1 from ICW1 to determine if single or cascade mode 
+        // In case of single mode: send flag to ISR to send vector of current interrupt to data buffer
         if(ICW1[1])begin
             send_vector_ISR=1;
             cascade_signal=0;
@@ -134,6 +143,10 @@ module ControlLogic(
             read_IRR=0;
             read_IMR=0;
         end
+        // In case of cascade mode: we must first check if the current PIC is master or slave
+        // based on the SP signal sent from cascade controller.
+        // In case of current pic is master: Check if the interrupt index is for slave or not
+        // if it's for slave send flag to cascade controller with the slave ID to handle it
         else begin
             if(SP==MASTER)begin
                 if(ICW3[interrupt_index])begin
@@ -141,6 +154,7 @@ module ControlLogic(
                 cascade_signal=1;
                 desired_slave=interrupt_index;
             end
+            // if it's not for slave then send flag to ISR to send vector of current interrupt to data buffer
             else begin
                 send_vector_ISR=1;
                 cascade_signal=0;
@@ -149,28 +163,34 @@ module ControlLogic(
                 read_IMR=0;
             end
             end
+            // In case of current pic is slave
+            // must check if there's a flag from cascade controller that the current slave should send it's vector
             else if(SP==SLAVE)begin
+                // There's coming flag from cascade controller then send the vector
                 if(cascade_flag) begin
                     send_vector_ISR=1;
                     read_ISR=0;
                     read_IRR=0;
                     read_IMR=0;
-                    cascade_flag_ACK=~cascade_flag_ACK;
+                    cascade_flag_ACK=~cascade_flag_ACK; // send ACK for cascade_flag 
                 end
+                // There's not coming flag from cascade controller then dont send the vector (There's another slave will send)
                 else begin
                     send_vector_ISR=0;
                 end
             end       
             end
     end
+    // reset flag sent to cascade controller when recieving acknowledge
     always @(cascade_signal_ACK)begin
         cascade_signal=1'b0;
     end
+    // reset flag sent to ISR when recieving acknowledge
     always @(send_vector_ISR_ACK)begin
         send_vector_ISR=1'b0;
     end
 
-    // Ack of read_priority flag to deactivate it 
+    // reset read_priority flag when recieving acknowledge
     always@(read_priority_ACK)
     begin
         read_priority = 1'b0; 
